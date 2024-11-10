4ad938878f8168c7ff421ed9b2bcc2d71c88f010
Joeffison Silvério de Andrade <joeffison@gmail.com>
2024-11-10 07:51:59 +0100

    git(linters): add linters

--- plant_swap_service/src/plant_swap/main.py
+++ plant_swap_service/src/plant_swap/main.py
@@ -1,20 +1,21 @@
 import uuid
 
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return 'OK'
+    return "OK"
 
 
 @app.get("/plants/{plant_uuid}")
 async def get_plant(plant_uuid: str):
-    """ Get a plant details by its uuid. """
-    if plant_uuid in ['1', str(uuid.UUID(int=0))]:
-        return {'plant_id': plant_uuid, 'plant_name': 'Thai'}
+    """Get a plant details by its uuid."""
+    if plant_uuid in ["1", str(uuid.UUID(int=0))]:
+        return {"plant_id": plant_uuid, "plant_name": "Thai"}
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail=f'Plant "{plant_uuid}" not found')
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f'Plant "{plant_uuid}" not found'
+    )
